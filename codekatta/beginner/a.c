868a1223e5d6c3219f0ec22f43e1e5b8eab6fe87
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2017-12-19 16:48:46 +0530

    Update a.c

--- codekatta/beginner/a.c
+++ codekatta/beginner/a.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 int main(void)
 {
-  int a[5],i,j=0;
-  for(i=0;i<5;i++)
+    int l;
+    scanf("%d",&l);
+  int a[l],i,j=0,;
+  for(i=0;i<l;i++)
   scanf("%d",&a[i]);
-    for(i=0;i<5;i++)
+    for(i=0;i<l;i++)
     {
       if(a[j]==a[i+1])
          {
            printf("\n%d",a[j]);
+           for(j=i+1;j<l;j++)
+           a[j]=a[j+1];
+           a[l]='\0';
+           l--;
                   }
          j++;
          }
