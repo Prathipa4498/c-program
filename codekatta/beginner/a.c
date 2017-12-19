0721287fc785285cf6ebb908020584926bfdd9cb
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2017-12-19 16:30:09 +0530

    Update a.c

--- codekatta/beginner/a.c
+++ codekatta/beginner/a.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+int main(void)
+{
+  int a[5],i,j=0;
+  for(i=0;i<5;i++)
+  scanf("%d",&a[i]);
+    for(i=0;i<5;i++)
+    {
+      if(a[j]==a[i+1])
+         {
+           printf("\n%d",a[j]);
+                  }
+         j++;
+         }
+         }
+         
+                  
+    
