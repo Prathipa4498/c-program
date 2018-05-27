fa7c6b91db349373431c89f7705e033dd1fc80b3
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2018-05-27 10:34:53 +0530

    Update prog7.c

--- codekatta/hunter/set 3/prog7.c
+++ codekatta/hunter/set 3/prog7.c
@@ -3,13 +3,13 @@ void main()
 {
 	int n,i,j;
 	scanf("%d",&n);
-	int a[n];
+	char a[n];
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%c",&a[i]);
 	}
 	if(a[0]==a[n-1])
 	n=n-1;
 	for(i=0;i<n;i++)
-	printf("%d",a[i]);
+	printf("%c",a[i]);
 }
