216c050f960609613876a2f21937055e1f4f5a9f
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2018-06-01 09:07:07 +0530

    Update prog5.c

--- codekatta/beginner/set4/prog5.c
+++ codekatta/beginner/set4/prog5.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+void main()
+{
+	int i=0,count=0;
+	char str[10000];
+	gets(str);
+	while(str[i]!='\0')
+	{
+		if(str[i]>47&&str[i]<58)
+		count++;
+		i++;
+	}
+	printf("%d",count);
+}
