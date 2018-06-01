f2dc3c026ad8d02ae552da1575be81764a23df95
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2018-06-01 09:07:34 +0530

    Update prog6.c

--- codekatta/beginner/set4/prog6.c
+++ codekatta/beginner/set4/prog6.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+void main()
+{
+	int i=0,count=0;
+	char str[10000];
+	gets(str);
+	while(str[i]!='\0')
+	{
+		if(str[i]<47||(str[i]>58&&str[i]<64)||(stsr[i]>90&&str[i]<97)||str[i]>122)
+		count++;
+		i++;
+	}
+	printf("%d",count);
+}
