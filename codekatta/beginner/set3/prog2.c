31e2da280b0a6e32fb75847c3b742bb2eb500d9b
jeevaad97 <34646490+jeevaad97@users.noreply.github.com>
2018-06-01 08:52:39 +0530

    Create prog2.c

---
+++ codekatta/beginner/set3/prog2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,k;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	k=a[0];
+	for(i=1;i<n;i++)
+	{
+		if(a[i]>k)
+		k=a[i];
+	}
+	printf("%d",k);
+}
